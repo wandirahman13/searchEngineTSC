cd686ffb5f2a314055432f55705f1b20210195e4
wandirahman13 <wandirahman13@gmail.com>
2020-04-08 03:42:58 +0700

    Create a basic homepage

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-It's Work!
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Wandering!</title>
+</head>
+<body>
+
+	<div class="mainSection">
+		
+		<div class="logoContainer">
+			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+		</div>
+
+		<div class="searchContainer">
+			<input class="searchBox" type="text" name="term" placeholder="Enter a keyword here...">
+			<input class="searchButton" type="submit" value="Search">
+		</div>
+
+	</div>
+
+</body>
+</html>
