0eb39ba26eac11a6bfdf9fee072a49798a641046
wandirahman13 <wandirahman13@gmail.com>
2020-04-08 05:00:06 +0700

    Styling container to the center

--- index.php
+++ index.php
@@ -6,19 +6,21 @@
 </head>
 <body>
 
-	<div class="mainSection">
-		
-		<div class="logoContainer">
-			<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-		</div>
+	<div class="wrapper indexPage">
+		<div class="mainSection">
+			
+			<div class="logoContainer">
+				<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+			</div>
 
-		<div class="searchContainer">
-			<form action="search.php" method="GET">
-				<input class="searchBox" type="text" name="term" placeholder="Enter a keyword here...">
-				<input class="searchButton" type="submit" value="Search">
-			</form>
-		</div>
+			<div class="searchContainer">
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="term" placeholder="Enter a keyword here...">
+					<input class="searchButton" type="submit" value="Search">
+				</form>
+			</div>
 
+		</div>
 	</div>
 
 </body>
