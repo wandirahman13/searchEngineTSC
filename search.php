1db0957a6af1442eac6df259c49666f8ce79ce92
wandirahman13 <wandirahman13@gmail.com>
2020-04-09 14:26:03 +0700

    Creating search page structure

--- search.php
+++ search.php
@@ -1,5 +1,41 @@
-<?php 
-	
-echo $_GET["term"]
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Wandering?</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
 
- ?>
+	<div class="wrapper indexPage">
+		
+		<div class="header">
+			
+			<div class="headerContent">
+				
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/wandering.png">
+					</a>
+				</div>
+
+				<div class="searchContainer">
+					<form action="search.php" method="GET">
+						
+						<div class="searchBarContainer">
+							<input class="searchBox" type="text" name="term">
+							<button>
+								search
+							</button>
+						</div>
+
+					</form>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
