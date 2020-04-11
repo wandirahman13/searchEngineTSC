d0b4d341a5dfd84b34a573be8c67cc09aef5b56e
wandirahman13 <wandirahman13@gmail.com>
2020-04-12 02:46:57 +0700

    add some method for loading the HTML of a site

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -1,8 +1,18 @@
 <?php 
 class DomDocumentParser {
 
+	private $doc;
+
 	public function __construct($url){
-		echo "URL: $url";
+
+		$option = array(
+			'http' => array('method'=>"GET", 'header'=>"User-Agent: wanderingBot/0.1\n") 
+			);
+		$context = stream_context_create($option);
+
+		$this->doc = new DomDocument();
+		$this->doc -> loadHTML(file_get_contents($url, false, $context));
+
 	}
 }
  ?>
