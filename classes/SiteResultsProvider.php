01adfa17d4e30ae551db32ebb4942227a076ab71
wandirahman13 <wandirahman13@gmail.com>
2020-04-15 03:16:14 +0700

    Outputting site result

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -27,6 +27,35 @@ class SiteResultsProvider {
 	}
 
 
+	public function getResultsHtml($page, $pageSize, $term){
+
+		$query = $this->con->prepare("SELECT * FROM sites_tb 
+										WHERE title LIKE :term
+										OR url LIKE :term
+										OR keywords LIKE :term
+										OR description LIKE :term
+										OR entity LIKE :term");
+
+		$searchTerm = "%" . $term . "%";
+		$query->bindParam(":term", $searchTerm);
+		$query->execute();
+
+		$resultsHtml = "<div class='sitesResults'>";
+
+
+		while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+			# code...
+			$title = $row["title"];
+			$resultsHtml .= "$title <br>";
+		}
+
+
+		$resultsHtml .= "</div>";
+
+		return $resultsHtml;
+	}
+
+
 
 }
 
